1329f8872a27d13e67d68281ee2156072febce24
Daniel Peach <daniel.peach@armory.io>
2023-01-26 13:41:34 -0500

    fix: make ui work from container (#3)

--- ui/vite.config.prod.ts
+++ ui/vite.config.prod.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/prod/ui/",
+  base: "/ui/",
   plugins: [react()],
   build: {
     outDir: 'dist/prod'
--- ui/vite.config.staging.ts
+++ ui/vite.config.staging.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/staging/ui/",
+  base: "/ui/",
   plugins: [react()],
   build: {
     outDir: 'dist/staging'
